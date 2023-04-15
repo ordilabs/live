24e0a31293cea5582c842746ed4865835c228b39
FelixWeis <felixweis@users.noreply.github.com>
2023-04-15 09:45:42 +0800

    setting site_addr via Cargo.toml, .env or ENV

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use cfg_if::cfg_if;
 cfg_if! { if #[cfg(feature = "ssr")] {
     use leptos::*;
     use crate::app::*;
-    //mod server_actions;
     mod backend;
     use backend::Backend;
     extern crate ord_mini;
@@ -17,7 +16,7 @@ cfg_if! { if #[cfg(feature = "ssr")] {
 
     use axum::body::Body as AxumBody;
     use axum::{
-        extract::{Extension, Path},
+        extract::{Extension, Path, },
         http::Request,
         response::{IntoResponse, Response},
         routing::{get, post},
@@ -32,6 +31,7 @@ cfg_if! { if #[cfg(feature = "ssr")] {
     use axum::{
         extract::TypedHeader,
         response::sse::{Event, Sse},
+
     };
     use futures::stream::{self, Stream};
     use std::{convert::Infallible, time::Duration};
@@ -60,10 +60,6 @@ async fn custom_handler(
     handler(req).await.into_response()
 }
 
-// #[get("/style.css")]
-// async fn css() -> impl Responder {
-//     actix_files::NamedFile::open_async("./style/output.css").await
-// }
 #[cfg(feature = "ssr")]
 async fn sse_handler(
     TypedHeader(user_agent): TypedHeader<headers::UserAgent>,
@@ -102,20 +98,6 @@ async fn main() {
         .init();
 
     crate::app::register_server_functions();
-
-    // let app = Router::new()
-    // .fallback_service(static_files_service)
-    // .route("/sse", get(sse_handler))
-    // .layer(TraceLayer::new_for_http());
-
-    // // Setting this to None means we'll be using cargo-leptos and its env vars.
-    // // when not using cargo-leptos None must be replaced with Some("Cargo.toml")
-
-    // let conf = leptos::get_configuration(Some("Cargo.toml")).await.unwrap();
-
-    // let addr = conf.leptos_options.site_addr.clone();
-    // let routes = generate_route_list(|cx| view! { cx,
-    //    <App/> });
 
     let mut ordipool: HashMap<String, Option<Inscription>> = HashMap::new();
     let backend = std::env::var("BACKEND")
@@ -156,11 +138,13 @@ async fn main() {
         }
     });
 
-    // build our application with a route
-    let conf = get_configuration(Some("Cargo.toml")).await.unwrap();
+    // Setting this to None means we'll be using cargo-leptos and its env vars.
+    // when not using cargo-leptos None must be replaced with Some("Cargo.toml")
+    let conf = get_configuration(None).await.unwrap();
     let leptos_options = conf.leptos_options;
     let routes = generate_route_list(|cx| view! { cx, <App/> }).await;
 
+    // build our application with a route
     let app = Router::new()
         .route("/api/events", get(sse_handler))
         .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
@@ -168,34 +152,13 @@ async fn main() {
         .route("/preview/:inscription_id", get(server_actions::preview))
         .route("/content/:inscription_id", get(server_actions::content))
         .leptos_routes(leptos_options.clone(), routes, |cx| view! { cx, <App/> })
+        //todo punks_ fallback
         .fallback(fallback::file_and_error_handler)
-        .layer(Extension(Arc::new(leptos_options)))
+        .layer(Extension(Arc::new(leptos_options.clone())))
         .layer(TraceLayer::new_for_http());
 
-    // run it
-    // HttpServer::new(move || {
-    //     let leptos_options = &conf.leptos_options;
-    //     let site_root = &leptos_options.site_root;
-
-    //     App::new()
-    //         .service(counter_events)
-    //         .service(web::rcargoesource("/preview/{inscription_id}").to(server_actions::preview))
-    //         .service(web::resource("/content/{inscription_id}").to(server_actions::content))
-    //         .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
-    //         .leptos_routes(leptos_options.to_owned(), routes.to_owned(), |cx| view! { cx, <App/> })
-    //         .service(Files::new("/punks/", "/tmp/punks"))
-    //         .service(Files::new("/", &site_root))
-    //         //.wrap(middleware::Compress::default())
-    // })
-    // .bind(&addr)?
-    // .run()
-    // .await
-
-    // let addr = "[::]:3000".parse().unwrap();
-    let addr = "[::]:3000".parse::<std::net::SocketAddr>().unwrap();
-
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
+    tracing::debug!("listening on {}", leptos_options.site_addr);
+    axum::Server::bind(&leptos_options.site_addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
