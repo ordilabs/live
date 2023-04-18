c17117a7d34e0195bc2320b74c2ae9f2d3ff6920
Jens K <47693+sectore@users.noreply.github.com>
2023-04-18 03:04:26 +0200

    Resolve: Toggle dark/light mode (#82)
    * Resolve: Toggle dark/light mode
    
    * Simplify things
    
    * Fix full height
    
    * Another fix for bg-colors

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,11 @@
 use crate::app::components::*;
-// use crate::app::pages::*;
+use crate::app::providers::*;
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
 mod components;
+mod providers;
 
 #[cfg(feature = "ssr")]
 use std::sync::atomic::{AtomicI32, Ordering};
@@ -18,6 +19,7 @@ pub fn register_server_functions() {
     _ = AdjustServerCount::register();
     _ = ClearServerCount::register();
     _ = GetLastInscription::register();
+    _ = SetDarkTheme::register();
 }
 
 #[cfg(feature = "ssr")]
@@ -33,7 +35,7 @@ pub enum LiveEvent {
 
 #[cfg(feature = "ssr")]
 lazy_static::lazy_static! {
-   pub static ref COUNT_CHANNEL: BroadcastChannel<i32> = BroadcastChannel::new();
+    pub static ref COUNT_CHANNEL: BroadcastChannel<i32> = BroadcastChannel::new();
     pub static ref INSCRIPTION_CHANNEL: BroadcastChannel<String> = BroadcastChannel::new();
     pub static ref EVENT_CHANNEL: BroadcastChannel<LiveEvent> = BroadcastChannel::new();
 }
@@ -64,9 +66,29 @@ pub async fn clear_server_count() -> Result<i32, ServerFnError> {
     Ok(0)
 }
 
+#[server(SetDarkTheme, "/api")]
+pub async fn set_dark_theme(cx: Scope, is_dark: bool) -> Result<bool, ServerFnError> {
+    use axum::http::header::{HeaderMap, HeaderValue, SET_COOKIE};
+    use leptos_axum::{ResponseOptions, ResponseParts};
+
+    let response =
+        use_context::<ResponseOptions>(cx).expect("to have leptos_axum::ResponseOptions provided");
+    let mut response_parts = ResponseParts::default();
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        SET_COOKIE,
+        HeaderValue::from_str(&format!("darkmode={is_dark}; Path=/")).expect("to create header value"),
+    );
+    response_parts.headers = headers;
+
+    response.overwrite(response_parts);
+    Ok(is_dark)
+}
+
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
     provide_meta_context(cx);
+    provide_theme_context(cx);
 
     #[cfg(not(feature = "ssr"))]
     let (multiplayer_value, info_value) = {
@@ -110,38 +132,38 @@ pub fn App(cx: Scope) -> impl IntoView {
 
     let initial_items: Vec<_> = (0..6).map(|n| format!("punk_{}.webp", n)).collect();
 
+    let theme_ctx =
+        use_context::<ThemeContext>(cx).expect("Failed to get ThemeContext");
+
     view! {
         cx,
-        <Body class="bg-white dark:bg-slate-800" />
         <Router>
-        <Meta name="twitter:card" content="summary_large_image"/>
-        <Meta name="twitter:site" content="@Ordilabs_org"/>
-        <Meta name="twitter:title" content="Ordinals Live"/>
-        <Meta name="twitter:description" content="Ordinals mempool viewer. View inscriptions before they're inscribed!"/>
-        <Meta name="twitter:image" content="https://live.ordilabs.org/ordilabs-logo-name-white.png"/>
-        <Link rel="shortcut icon" href="/favicon.png"/>
-        <Stylesheet id="leptos" href="/pkg/ordilabs_live.css"/>
-
-        <div class="flex h-full">
-        <div class="flex flex-1 flex-col overflow-hidden">
-           <Header />
-            <div class="flex flex-1 items-stretch overflow-hidden">
-                <main class="flex-1 overflow-y-auto">
+          <Meta name="twitter:card" content="summary_large_image"/>
+          <Meta name="twitter:site" content="@Ordilabs_org"/>
+          <Meta name="twitter:title" content="Ordinals Live"/>
+          <Meta name="twitter:description" content="Ordinals mempool viewer. View inscriptions before they're inscribed!"/>
+          <Meta name="twitter:image" content="https://live.ordilabs.org/ordilabs-logo-name-white.png"/>
+          <Link rel="shortcut icon" href="/favicon.png"/>
+          <Stylesheet id="leptos" href="/pkg/ordilabs_live.css"/>
+          <body class:dark=move || theme_ctx.is_dark.get() class="h-screen">
+            <div class="flex h-full flex-1 flex-col overflow-hidden bg-white dark:bg-slate-800">
+              <Header />
+              <div class="flex flex-1 items-stretch overflow-hidden">
+                  <main class="flex-1 overflow-y-auto">
                     <div class="mx-auto max-w-7xl px-4 pt-8 sm:px-6 lg:px-8">
-                        <div class="flex">
-                            <h1 class="flex-1 text-2xl font-bold text-gray-900 dark:text-gray-100">
-                                "Live unconfirmed inscriptions"
-                            </h1>
-
-                        </div>
-                        <div class="text-xs text-gray-900 dark:text-gray-100">{info_value}</div>
-                        <LiveGrid initial_items multiplayer_value/>
+                      <div class="flex">
+                        <h1 class="flex-1 text-2xl font-bold text-gray-900 dark:text-gray-100">
+                            "Live unconfirmed inscriptions"
+                        </h1>
+                      </div>
+                      <div class="text-xs text-gray-900 dark:text-gray-100">{info_value}</div>
+                      <LiveGrid initial_items multiplayer_value/>
                     </div>
-                </main>
+                  </main>
+              </div>
+              <Footer />
             </div>
-            <Footer />
-        </div>
-    </div>
+          </body>
         </Router>
     }
 }
