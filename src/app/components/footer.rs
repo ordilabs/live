4693495a14ef6991a1d1bb6939105b3d32b3ba3a
Jens Krause <email@jkrause.io>
2023-05-13 08:49:01 +0200

    Enable Hi

--- src/app/components/footer.rs
+++ src/app/components/footer.rs
@@ -81,7 +81,6 @@ pub fn Footer(cx: Scope) -> impl IntoView {
                 onchange="this.form.requestSubmit()"
               >
                 {Locale::iter()
-                    .filter(|l| *l != Locale::Hi)
                     .map(|l| {
                         let value = l.as_ref();
                         let label = l.to_label();
